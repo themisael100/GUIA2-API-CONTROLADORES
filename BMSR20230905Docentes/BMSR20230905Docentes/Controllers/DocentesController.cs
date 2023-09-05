0aacae7d4b24f8af5327c17d0b9dcd7336fb0479
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-05 15:29:26 -0600

    Craecion del segundo Ejercicio

---
+++ BMSR20230905Docentes/BMSR20230905Docentes/Controllers/DocentesController.cs
@@ -0,0 +1,27 @@
+﻿using BMSR20230905Docentes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BMSR20230905Docentes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocentesController : ControllerBase
+    {
+        static List<Docentes> Docente = new List<Docentes>();
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Docentes docente)
+        {
+            Docente.Add(docente);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public Docentes Get(int id)
+        {
+            var docentes = Docente.FirstOrDefault(c => c.Id == id);
+            return docentes;
+        }
+    }
+}
---
+++ BMSR20230905Docentes/BMSR20230905Docentes/Models/Docentes.cs
@@ -0,0 +1,10 @@
+﻿namespace BMSR20230905Docentes.Models
+{
+    public class Docentes
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string subject { get; set; }
+    }
+}
