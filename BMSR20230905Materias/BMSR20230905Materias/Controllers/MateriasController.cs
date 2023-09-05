db7baa0ad2086a0eae3d01b55cfbd2b9ffc74f20
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-05 16:21:27 -0600

    Craecion del tercer Ejercicio

---
+++ BMSR20230905Materias/BMSR20230905Materias/Controllers/MateriasController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BMSR20230905Materias.Models;
+
+namespace BMSR20230905Materias.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MateriasController : ControllerBase
+    {
+        static List<Materias> materias = new List<Materias>();
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Materias materia)
+        {
+            materias.Add(materia);
+            return Ok();
+        }
+        [HttpGet]
+        public IEnumerable<Materias> Get()
+        {
+            return materias;
+        }
+        [HttpDelete("{id}")]
+
+
+        public IActionResult Delete(int id)
+        {
+            var existingMateria = materias.FirstOrDefault(M => M.Id == id);
+            if (existingMateria != null)
+            {
+
+                materias.Remove(existingMateria);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ BMSR20230905Materias/BMSR20230905Materias/Models/Materias.cs
@@ -0,0 +1,8 @@
+﻿namespace BMSR20230905Materias.Models
+{
+    public class Materias
+    {
+        public int Id {  get; set; }
+        public string Subject { get; set; }
+    }
+}
