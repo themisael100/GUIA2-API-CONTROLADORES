72d506790188b92b9a7934824a67703cbeb33be5
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-05 16:35:12 -0600

    Creacion del tercer Ejercicio

---
+++ BMSR20230905Aulas/BMSR20230905Aulas/Controllers/AulaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BMSR20230905Aulas.Models;
+
+namespace BMSR20230905Aulas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AulaController : ControllerBase
+    {
+        private readonly List<Aulas> aulas;
+
+        public AulaController()
+        {
+            aulas = new List<Aulas>
+            {
+                new Aulas { Id = 1, classroom = "Aula Sociles" },
+                new Aulas { Id = 2, classroom = "Aula Matematicas" },
+                new Aulas { Id = 3, classroom = "Aula Lenguaje" }
+               
+            };
+        }
+
+        [HttpGet]
+        public IActionResult ObtenerTodasLasAulas()
+        {
+            return Ok(aulas);
+        }
+    }
+
+}
+
---
+++ BMSR20230905Aulas/BMSR20230905Aulas/Models/Aulas.cs
@@ -0,0 +1,8 @@
+﻿namespace BMSR20230905Aulas.Models
+{
+    public class Aulas
+    {
+        public int Id {  get; set; }
+        public string classroom { get; set; }
+    }
+}
