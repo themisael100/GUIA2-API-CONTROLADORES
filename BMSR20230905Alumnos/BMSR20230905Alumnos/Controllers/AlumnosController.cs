b81edb261008ef23b7f9ef2f1bab459907b7f298
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-05 15:04:13 -0600

    Creacion del primer Ejercicio

---
+++ BMSR20230905Alumnos/BMSR20230905Alumnos/Controllers/AlumnosController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using BMSR20230905Alumnos.Models;
+namespace BMSR20230905Alumnos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnosController : ControllerBase
+    {
+        static List<Alumnos> alumno = new List<Alumnos>();
+
+        [HttpGet]
+        public IEnumerable<Alumnos> Get()
+        {
+            return alumno;
+        }
+        [HttpGet("{id}")]
+        public Alumnos Get(int id)
+        {
+            var alumnos = alumno.FirstOrDefault(c => c.Id == id);
+            return alumnos;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Alumnos alumnos)
+        {
+            alumno.Add(alumnos);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Put(int id, [FromBody] Alumnos alumnos)
+        {
+            var existingAlumno = alumno.FirstOrDefault(a => a.Id == id);
+            if (existingAlumno != null)
+            {
+
+                existingAlumno.Name = alumnos.Name;
+                existingAlumno.LastName = alumnos.LastName;
+                existingAlumno.Age = alumnos.Age;
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(int id)
+        {
+            var existingAlumno = alumno.FirstOrDefault(a => a.Id == id);
+            if (existingAlumno != null)
+            {
+
+                alumno.Remove(existingAlumno);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
+
---
+++ BMSR20230905Alumnos/BMSR20230905Alumnos/Models/Alumnos.cs
@@ -0,0 +1,10 @@
+﻿namespace BMSR20230905Alumnos.Models
+{
+    public class Alumnos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
