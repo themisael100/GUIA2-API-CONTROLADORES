381615c061fe743a9652bccc5c9095977233c6d7
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-05 14:43:56 -0600

    Ejercicio de practica

---
+++ Practica/Practica/Controllers/ClientsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practica.Models;
+
+namespace Practica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        static List<Client> clients = new List<Client>();
+
+        [HttpGet]
+        public IEnumerable<Client> Get()
+        {
+            return clients;
+        }
+        [HttpGet("{id}")]
+        public Client Get(int id)
+        {
+            var client = clients.FirstOrDefault(c => c.Id == id);
+            return client;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Client client)
+        {
+            clients.Add(client);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Put(int id ,[FromBody] Client client) 
+        {
+            var existingClient = clients.FirstOrDefault(c => c.Id == id);
+            if (existingClient != null)
+            {
+       
+                existingClient.Name = client.Name;
+                existingClient.lastName = client.lastName;
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(int id)
+        {
+            var existingClient = clients.FirstOrDefault(c => c.Id == id);
+            if (existingClient != null)
+            {
+              
+                clients.Remove(existingClient);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Practica/Practica/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace Practica.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string lastName { get; set; }
+
+    }
+}
